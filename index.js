a369a3591e6a1cc33be4965f5fca1937154d2f08
Mohamed Bankolé <medbankole97@gmail.com>
2024-10-04 10:55:09 +0000

    add helmet and cors

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 import express from "express";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import cors from 'cors';
 const app = express();
+app.use(helmet());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  });
+app.use(limiter);
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello world' });
 });
+
+const corsOptions = {
+    origin: 'http://example.com',
+  };
+  
+  app.use(cors(corsOptions));
+  
+  app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello world' });
+  });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
