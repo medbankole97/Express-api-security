e16a9cfe25847d48329b7c69430432ab21e81712
Mohamed Bankolé <medbankole97@gmail.com>
2024-10-04 10:24:34 +0000

    add endpoint

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello world' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
